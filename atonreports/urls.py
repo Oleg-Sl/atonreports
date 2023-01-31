2eb013df18f1504ea2cccb68684610a453ab39ed
Oleg <slepcov_oleg@mail.ru>
2023-02-01 00:13:33 +0700

    Added prefix urls

--- atonreports/urls.py
+++ atonreports/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('reports/admin/', admin.site.urls),
 
-    path('dpk/api/v1/', include('dpk_api_v1.urls', namespace='dpk_api_v1')),
-    path('calls-statistic/api/v1/', include('cs_api_v1.urls', namespace='cs_api_v1')),
+    path('reports/dpk/api/v1/', include('dpk_api_v1.urls', namespace='dpk_api_v1')),
+    path('reports/calls-statistic/api/v1/', include('cs_api_v1.urls', namespace='cs_api_v1')),
 
 ]
