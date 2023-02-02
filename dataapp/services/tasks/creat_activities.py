7aae9a122b046e02dd8653f8a4dfa8c528a4e73a
Oleg <slepcov_oleg@mail.ru>
2023-02-02 16:48:04 +0700

    Changed model Deals

--- dataapp/services/tasks/creat_activities.py
+++ dataapp/services/tasks/creat_activities.py
@@ -17,7 +17,7 @@ def creat_and_update_activities(begin_date=None, end_date=None):
     if begin_date:
         filter_field["<CREATED"] = end_date
 
-    # filter_field[">CREATED"] = "2022-01-01"
+    filter_field[">CREATED"] = "2022-01-01"
     filter_field["TYPE_ID"] = "3"
 
         # "filter": {
