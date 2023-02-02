5c272e6c120f572130cd98e27aacf79a9f63092c
Oleg <slepcov_oleg@mail.ru>
2023-02-02 16:45:10 +0700

    Changed model Deals

--- dataapp/services/tasks/creat_activities.py
+++ dataapp/services/tasks/creat_activities.py
@@ -17,9 +17,8 @@ def creat_and_update_activities(begin_date=None, end_date=None):
     if begin_date:
         filter_field["<CREATED"] = end_date
 
-    filter_field[">CREATED"] = "2022-01-01"
-    filter_field["TYPE_ID"] = "2"
-    filter_field[">ID"] = 3604629
+    # filter_field[">CREATED"] = "2022-01-01"
+    filter_field["TYPE_ID"] = "3"
 
         # "filter": {
         #     "TYPE_ID": "2",
@@ -27,7 +26,7 @@ def creat_and_update_activities(begin_date=None, end_date=None):
         # },
 
     total = get_total(bx24, "crm.activity.list", filter_field)
-    add_activities_to_db(bx24, "crm.activity.list", filter_field, total, 0, 3604629)
+    add_activities_to_db(bx24, "crm.activity.list", filter_field)
 
 
 def get_total(bx24, method, filter_field={}):
