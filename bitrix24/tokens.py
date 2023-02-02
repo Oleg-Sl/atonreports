a94bf4e18bff1b1f46bcfd24db7811b4c0d88e6b
Oleg <slepcov_oleg@mail.ru>
2023-02-03 00:11:25 +0700

    Turn on distinct

--- bitrix24/tokens.py
+++ bitrix24/tokens.py
@@ -13,8 +13,13 @@ path_secret_file = os.path.join(settings.BASE_DIR, 'bitrix24', 'secrets_bx24.jso
 def save_secrets(data):
     print("FILE: ", path_secret_file)
     """ Запись токенов доступа к BX24 в файл """
+    data_old = {}
+    with open(path_secret_file) as secrets_file:
+        data_old = json.load(secrets_file)
+
     with open(path_secret_file, 'w') as secrets_file:
-        json.dump(data, secrets_file)
+        data_old.update(data)
+        json.dump(data_old, secrets_file)
         print("SUCCESS WRITING!!!")
 
 
