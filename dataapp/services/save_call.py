f07532a384fa5c01520ff5800f68892ebf2a49e0
Oleg <slepcov_oleg@mail.ru>
2023-02-05 00:11:43 +0700

    Changed save calls to db

--- dataapp/services/save_call.py
+++ dataapp/services/save_call.py
@@ -6,6 +6,7 @@ def add_call_drf(call):
     activity_obj = Activity.objects.filter(ID=call.get("CRM_ACTIVITY_ID")).first()
     user_obj = User.objects.filter(ID=call.get("PORTAL_USER_ID")).first()
 
+    call["CALL_DURATION"] = call.get("CALL_DURATION") if call.get("CALL_DURATION") else 0
     call["CRM_ACTIVITY_ID"] = activity_obj.pk if activity_obj else None
     call["PORTAL_USER_ID"] = user_obj.pk if user_obj else None
 
