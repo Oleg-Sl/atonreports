ef0df9a51b8c0fe4b7e10070e494a4f69e0c8d5e
Oleg <slepcov_oleg@mail.ru>
2023-02-05 12:57:00 +0700

    Changed active_by_month

--- cs_api_v1/filter_queryset.py
+++ cs_api_v1/filter_queryset.py
@@ -19,19 +19,18 @@ class CallsFilter(filters.FilterSet):
     CREATED = filters.DateFromToRangeFilter()
     CALL_START_DATE = filters.DateFromToRangeFilter(field_name='CALL_START_DATE')
     DURATION = filters.NumberFilter(field_name='DURATION', lookup_expr='gte')
-    RESPONSIBLE_ID = filters.NumberFilter(field_name='RESPONSIBLE_ID__ID', lookup_expr='eq')
+    # RESPONSIBLE_ID = filters.NumberFilter(field_name='RESPONSIBLE_ID__ID', lookup_expr='eq')
 
     class Meta:
         model = Activity
-        fields = ["RESPONSIBLE_ID", "CREATED", "DURATION", "CALL_START_DATE"]
+        fields = ["RESPONSIBLE_ID__ID", "CREATED", "DURATION", "CALL_START_DATE"]
 
 
 class CommentFilter(filters.FilterSet):
     date_comment = filters.DateFromToRangeFilter()
-    recipient = filters.NumberFilter(field_name='recipient__ID', lookup_expr='eq')
-    commentator = filters.NumberFilter(field_name='commentator__ID', lookup_expr='eq')
+    # recipient = filters.NumberFilter(field_name='recipient__ID', lookup_expr='eq')
+    # commentator = filters.NumberFilter(field_name='commentator__ID', lookup_expr='eq')
 
     class Meta:
         model = Comment
-        fields = ["recipient", "commentator", "date_comment", ]
-
+        fields = ["recipient__ID", "commentator__ID", "date_comment", ]
