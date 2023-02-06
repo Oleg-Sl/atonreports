ee56692f162d51a9115691908501c72b0c230c3c
Oleg <slepcov_oleg@mail.ru>
2023-02-06 12:30:26 +0700

    Started dpk

--- dpk_api_v1/views.py
+++ dpk_api_v1/views.py
@@ -200,23 +200,23 @@ class StatisticCompanyViewSet(viewsets.GenericViewSet):
     #     duration = self.request.query_params.get("duration", "0")
     #     return super().get_queryset().statistic_company(duration)
 
-    # def list(self, request, *args, **kwargs):
-    #     duration = request.query_params.get("duration", "0")
-    #     if not duration.isdigit():
-    #         return Response('The "duration" value must be an integer', status=status.HTTP_400_BAD_REQUEST)
-    #
-    #     queryset = self.filter_queryset(
-    #         self.get_queryset()
-    #     )
-    #
-    #     page = self.paginate_queryset(queryset)
-    #     if page is not None:
-    #         serializer = self.get_serializer(page, many=True)
-    #         return self.get_paginated_response(serializer.data)
-    #
-    #     serializer = self.get_serializer(queryset, many=True)
-    #
-    #     return Response(serializer.data)
+    def list(self, request, *args, **kwargs):
+        duration = request.query_params.get("duration", "0")
+        if not duration.isdigit():
+            return Response('The "duration" value must be an integer', status=status.HTTP_400_BAD_REQUEST)
+
+        queryset = self.filter_queryset(
+            self.get_queryset()
+        )
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+
+        return Response(serializer.data)
 
 
 class StatisticCompanyDirectionViewSet(viewsets.GenericViewSet):
