5127f00af9f2c627c4c3be5fe4fca37261c26a05
Oleg <slepcov_oleg@mail.ru>
2023-03-20 10:16:26 +0700

    Changed command db_companies

--- dataapp/management/commands/db_companies.py
+++ dataapp/management/commands/db_companies.py
@@ -1,7 +1,9 @@
+import datetime
 from django.core.management.base import BaseCommand
 
 from dataapp.services.save_data_from_bx24 import all_companies
 from bitrix24.request import Bitrix24
+
 
 
 class Command(BaseCommand):
@@ -9,6 +11,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         bx24 = Bitrix24()
-        all_companies.save_to_db(bx24)
+
+        all_companies.save_to_db(bx24, datetime.date(2023, 3, 10))
 
 
--- dataapp/services/save_data_from_bx24/all_companies.py
+++ dataapp/services/save_data_from_bx24/all_companies.py
@@ -1,32 +1,40 @@
 import sys
 sys.setrecursionlimit(2000)
+import datetime
 
 from .. import save_company
 
 
-def save_to_db(bx24):
-    total_companies = get_total(bx24, "crm.company.list", {})
-    save_companies_to_db(bx24, total_companies)
+def save_to_db(bx24, date_from=datetime.datetime.now() - datetime.timedelta(365), date_to=datetime.datetime.now() + datetime.timedelta(1)):
+    filter_data = {
+        ">DATE_CREATE": date_from.strftime("%Y-%m-%d"),
+        "<DATE_CREATE": date_to.strftime("%Y-%m-%d")
+    }
+    total_companies = get_total(bx24, "crm.company.list", filter_data)
+    save_companies_to_db(bx24, filter_data, total_companies)
 
-    total_requisites = get_total(bx24, "crm.requisite.list", {"ENTITY_TYPE_ID": 4})
-    save_requisites_to_db(bx24, total_requisites)
+    filter_data["ENTITY_TYPE_ID"] = 4
+    total_requisites = get_total(bx24, "crm.requisite.list", filter_data)
+    save_requisites_to_db(bx24, filter_data, total_requisites)
 
-    total_addresses = get_total(bx24, "crm.address.list", {"ENTITY_TYPE_ID": 4})
-    save_addresses_to_db(bx24, total_addresses)
+    total_addresses = get_total(bx24, "crm.address.list", filter_data)
+    save_addresses_to_db(bx24, filter_data, total_addresses)
 
 
-def save_companies_to_db(bx24, total=0, count=0, id_start=0):
+def save_companies_to_db(bx24, filter_data, total=0, count=0, id_start=0):
+    filter_data[">ID"] = id_start
     params = {
         "select": [
             "ID", "ASSIGNED_BY_ID", "TITLE", "DATE_CREATE", "ADDRESS", "REVENUE", "INDUSTRY", "UF_CRM_1640828035",
             "UF_CRM_1639121988","UF_CRM_1639121612", "UF_CRM_1639121303", "UF_CRM_1639121341", "UF_CRM_1617767435",
             "UF_CRM_1639121225", "UF_CRM_1639121262"
         ],
-        "filter": {
-            ">ID": id_start,
+        "filter": filter_data,
+            # {
+            # ">ID": id_start,
             # ">DATE_CREATE": "2022-01-01",
             # "<DATE_CREATE": None
-        },
+        # },
         "order": {"ID": "ASC"},
         "start": -1
     }
@@ -42,16 +50,18 @@ def save_companies_to_db(bx24, total=0, count=0, id_start=0):
             print("OUTPUT: ", res)
 
         print(f"Получено {count} из {total}")
-        save_companies_to_db(bx24, total, count, id_start)
+        save_companies_to_db(bx24, filter_data, total, count, id_start)
 
 
-def save_requisites_to_db(bx24, total=0, count=0, id_start=0):
+def save_requisites_to_db(bx24, filter_data, total=0, count=0, id_start=0):
+    filter_data[">ID"] = id_start
     params = {
         "select": ["ID", "RQ_INN", "ENTITY_ID", ],
-        "filter": {
-            "ENTITY_TYPE_ID": 4,
-            ">ID": id_start
-        },
+        "filter": filter_data,
+        #     {
+        #     "ENTITY_TYPE_ID": 4,
+        #     ">ID": id_start
+        # },
         "order": {"ID": "ASC"},
         "start": -1
     }
@@ -66,13 +76,15 @@ def save_requisites_to_db(bx24, total=0, count=0, id_start=0):
             print("OUTPUT: ", res)
 
         print(f"Получено реквизитов {count} из {total}")
-        save_requisites_to_db(bx24, total, count, id_start)
+        save_requisites_to_db(bx24, filter_data, total, count, id_start)
 
 
-def save_addresses_to_db(bx24, total=0, count=0, id_start=0):
+def save_addresses_to_db(bx24, filter_data, total=0, count=0, id_start=0):
+    filter_data[">LOC_ADDR_ID"] = id_start
     params = {
         "select": ["LOC_ADDR_ID", "REGION", "CITY", "PROVINCE", "ENTITY_ID", ],
-        "filter": {"ENTITY_TYPE_ID": 4, ">LOC_ADDR_ID": id_start},
+        "filter": filter_data,
+        # "filter": {"ENTITY_TYPE_ID": 4, ">LOC_ADDR_ID": id_start},
         "order": {"LOC_ADDR_ID": "ASC"},
         "start": -1
     }
@@ -85,9 +97,8 @@ def save_addresses_to_db(bx24, total=0, count=0, id_start=0):
             address["ID"] = address.get("ENTITY_ID")
             res = save_company.update_company_drf(address)
             print("OUTPUT: ", res)
-
         print(f"Получено адресов {count} из {total}")
-        save_addresses_to_db(bx24, total, count, id_start)
+        save_addresses_to_db(bx24, filter_data, total, count, id_start)
 
 
 def get_total(bx24, method, filter_field={}):
