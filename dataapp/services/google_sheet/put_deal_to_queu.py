803a310ceda551488b20b44806e21a0ee80bb33d
Oleg <slepcov_oleg@mail.ru>
2023-04-16 10:30:59 +0700

    Added data to google handler

--- dataapp/services/google_sheet/put_deal_to_queu.py
+++ dataapp/services/google_sheet/put_deal_to_queu.py
@@ -13,11 +13,11 @@ def put(deal, fields, redis_conn):
     rop = User.objects.filter(ID=deal["UF_CRM_1529900092"]).values("LAST_NAME", "NAME").first()
     deal["rop"] = f"{rop['NAME']} {rop['LAST_NAME']}" if rop else None
 
-    deal["source"] = get_value(fields["UF_CRM_5DE0B9ADE7595"]["items"])
-    deal["source_dir"] = get_value(fields["UF_CRM_5DE0B9ACD162A"]["items"])
-    deal["source_id"] = get_value(fields["UF_CRM_SOURCE_ID"]["items"])
-    deal["crm_status"] = get_value(fields["UF_CRM_STATUS_ID"]["items"])
-    deal["direction"] = get_value(fields["UF_CRM_1610523951"]["items"])
+    deal["source"] = get_value(fields["UF_CRM_5DE0B9ADE7595"]["items"], deal["UF_CRM_5DE0B9ADE7595"])
+    deal["source_dir"] = get_value(fields["UF_CRM_5DE0B9ACD162A"]["items"], deal["UF_CRM_5DE0B9ACD162A"])
+    deal["source_id"] = get_value(fields["UF_CRM_SOURCE_ID"]["items"], deal["UF_CRM_SOURCE_ID"])
+    deal["crm_status"] = get_value(fields["UF_CRM_STATUS_ID"]["items"], deal["UF_CRM_STATUS_ID"])
+    deal["direction"] = get_value(fields["UF_CRM_1610523951"]["items"], deal["UF_CRM_1610523951"])
 
     redis_conn.rpush('googlequeue', deal)
 
