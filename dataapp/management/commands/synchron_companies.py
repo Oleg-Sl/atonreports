8f9ecbf7ebddcc011928ec537c937eb398451e0e
Oleg <slepcov_oleg@mail.ru>
2023-08-26 23:36:47 +0700

    Synchronization companies

--- dataapp/management/commands/synchron_companies.py
+++ dataapp/management/commands/synchron_companies.py
@@ -2,6 +2,7 @@ import datetime
 from django.core.management.base import BaseCommand
 
 from dataapp.services.save_data_from_bx24 import get_companies
+from dataapp.services import save_company
 from bitrix24.request import Bitrix24
 
 
@@ -11,5 +12,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         bx24 = Bitrix24()
         companies_ids = get_companies.get_id_companies_from_bx24(bx24)
-        print(companies_ids)
+        # print(companies_ids)
         print(len(companies_ids))
+        save_company.update_companies(companies_ids)
+
